c1784736f1502ff2e45b283780dd7f7d90dba092
Jakob Kaur <Jakob-kaur.kelder@voco.ee>
2024-09-16 11:24:25 +0300

    Write data to file

--- index.js
+++ index.js
@@ -6,20 +6,51 @@ const path = require("path")
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 
-app.get("/", (req, res) => {
+
+const readFile = (filename) => {
+  return new Promise((resolve, reject) => {
     fs.readFile("./tasks", "utf8", (err, data) => {
         if (err) {
           console.error(err);
-          return
+          return;
         }
-        console.log(data);
-        console.log(typeof data);
-        console.log(data.split("\n"))
-    const tasks = data.split("\n")
-    res.render("index", {tasks: tasks})
-  });
+        
+        const tasks = data.split("\n")
+        resolve(tasks)
+    });
+  })
+}
+
+
+app.get("/", (req, res) => {
+    readFile("./tasks")
+      .then(tasks => {
+        console.log(tasks)
+        res.render("index", {tasks: tasks})
+    })
 })
 
+
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => {
+    //tasks list data from file
+  readFile("./tasks")
+    .then(tasks => {
+        // add form sent task to array
+      tasks.push(req.body.task)
+      const data = tasks.join("\n")
+      fs.writeFile("./tasks", data, err => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        // redirect to see result
+        res.redirect("/")
+      })
+    })
+})
 
 app.listen(3001, () => {
     console.log("Example app is started at http://localhost:3001")
